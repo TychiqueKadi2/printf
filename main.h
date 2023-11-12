70caff7b4bdabe7b0c513cc74efedacf2ce5e8af
Tychiquekadi2 <tychiquemukadi@gmail.com>
2023-11-11 19:28:03 -0800

    prints x hexadecimal numbers

--- main.h
+++ main.h
@@ -1,18 +1,45 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <limits.h>
+#include <stdio.h>
 #include <stdarg.h>
-#include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define BUFFER_SIZE 1024
+/**
+ * struct format - converter for printf
+ * @ph: type char pointer of the specifier
+ * @function: function for the conversion specifier
+ *
+ */
+typedef struct format
+{
+	char *ph;
+	int (*function)();
+} convert;
 
+int *_strcpy(char *dest, char *src);
+int _print_point(va_list val);
+int _print_uint(va_list args);
+int _print_hexa_ex(unsigned long int num);
+int _strlenc(const char *s);
+int _print_HEXA_ex(unsigned int num);
+int _print_string(va_list val);
+int _print_HEXA(va_list val);
+int _print_hexa(va_list val);
+int _print_octa(va_list val);
+int _print_bi(va_list val);
+int _print_revs(va_list args);
+int _print_rot13(va_list args);
+int _print_i(va_list args);
+int _print_d(va_list args);
+int _strlen(char *s);
+int _rev_string(char *s);
+int _print_37(void);
+int _print_c(va_list val);
+int _print_s(va_list val);
 int _pchar(char c);
 int _printf(const char *format, ...);
-void print_bi(unsigned int uint);
-int print_buf(const char *input);
-int print_pt(va_list val);
-int print_hex_extra(unsigned long int num);
 
 #endif
---
+++ print_HEXA.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _print_HEXA - prints and hexadecimal number.
+ * @val: arguments.
+ * Return: count.
+ */
+int _print_HEXA(va_list val)
+{
+	int a;
+	int *ary;
+	int count = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int tem = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		count++;
+	}
+	count++;
+	ary = malloc(count * sizeof(int));
+
+	for (a = 0; a < count; a++)
+	{
+		ary[a] = tem % 16;
+		tem /= 16;
+	}
+	for (a = count - 1; a >= 0; a--)
+	{
+		if (ary[a] > 9)
+			ary[a] = ary[a] + 7;
+		_pchar(ary[a] + '0');
+	}
+	free(ary);
+	return (count);
+}
