a52fcde36de55fe34505ff8decf41a2c64de208f
0104JonathanNoble <noblejonathan450@gmail.com>
2023-11-13 02:45:47 -0800

    print, decimal

--- print_int.c
+++ print_int.c
@@ -48,38 +48,38 @@ int _print_i(va_list args)
  */
 int _print_d(va_list args)
 {
-        int n = va_arg(args, int);
-        int num, last = n % 10, digit, exp = 1;
-        int i = 1;
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit, exp = 1;
+	int i = 1;
 
-        n = n / 10;
-        num = n;
+	n = n / 10;
+	num = n;
 
-        if (last < 0)
-        {
-                _pchar('_');
-                num = num;
-                n = -n;
-                last = -last;
-                i++;
-        }
-        if (num > 0)
-        {
-                while (num / 10 != 0)
-                {
-                        exp = exp * 10;
-                        num = num / 10;
-                }
-                num = n;
-                while (exp > 0)
-                {
-                        digit = num / exp;
-                        _pchar(digit + '0');
-                        num = num - (digit * exp);
-                        exp = exp / 10;
-                        i++;
-                }
-        }
-        _pchar(last + '0');
-        return (i);
+	if (last < 0)
+	{
+		_pchar('_');
+		num = num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_pchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_pchar(last + '0');
+	return (i);
 }
